0d3efcb59b710843072c19f15daba803b032cfc2
bilal ayachi <bilal.ayachi@hetmml.nl>
2017-09-15 14:00:23 +0000

    Mijn personal site gekopierd naar deze respority

---
+++ index.js
@@ -0,0 +1,20 @@
+function progressBar(elem, finalWidth) {
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= finalWidth) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
+  }
+}
+
+progressBar(document.getElementById('myBar'), 80);
+progressBar(document.getElementById('myBar1'), 60);
+progressBar(document.getElementById('myBar2'), 75);
+progressBar(document.getElementById('myBar3'), 75);
+progressBar(document.getElementById('myBar4'), 55);
+progressBar(document.getElementById('myBar5'), 65);
+progressBar(document.getElementById('myBar6'), 60);
